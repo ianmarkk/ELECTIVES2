4e6af7eccfe9bb4742de1589be997618eae09e21
Ian Mark Asuncion <99246141+ianmarkk@users.noreply.github.com>
2022-02-17 15:32:40 +0800

    Add files via upload

---
+++ ELECT2-MOD1-MP01.php
@@ -0,0 +1,111 @@
+<!DOCTYPE hmtl>
+<html>
+    <head>
+        <title>
+                ELECT2-MOD1-MP01
+        </title>
+    </head>
+<body>
+
+<h2>PERSONAL INFORMATION</h2>
+<?php
+    $fullname = "Ian Mark Asuncion";
+    $gender = "Male";
+    $month = "April";
+    $home = "Pulung Cacutud, Angeles City";
+    $email = "asuncion.ianmark@auf.edu.ph";
+    define ("Day",27);
+    define ("Year",1999);
+    
+    echo "Name: ";
+    echo $fullname;
+    echo "<br> Birthday: ";
+    echo $month; echo " ",Day; echo ", ",Year;
+    echo "<br> Gender: ";
+    echo $gender;
+    echo "<br> Home Address: ";
+    echo $home;
+    echo "<br> Email Address: ";
+    echo $email;
+?>
+
+<h2>FAMILY BACKGROUND</h2>
+<?php
+    $lastname = "Pilar";
+    echo "Father's Name: Joel A. ";
+    echo $lastname;
+    echo "<br> Mother's Name: Jenny A. ";
+    echo $lastname;
+    echo "<br> Sibling 1: Luise Andrei A. ";
+    echo $lastname;
+?>
+
+<h2>EDUCATIONAL BACKGROUND</h2>
+<?php
+    $primary = 2012;
+    $jhs = 2016;
+    $shs = 2018;
+    $school = "Living Stone International School";
+    $tertiary = "Angeles University Foundation";
+    $course = "BS Computer Engineering";
+
+    echo "Primary School: ";
+    echo $school;
+    echo "<br> Date Graduated: Year ";
+    echo $primary;
+    echo "<br>";
+    
+    echo "<p>Junior High School: ";
+    echo $school;
+    echo "<br> Date Graduated: Year ";
+    echo $jhs;
+    echo "<br>";
+
+    echo "<p>Senior High School: ";
+    echo $school;
+    echo "<br> Date Graduated: Year ";
+    echo $shs;
+    echo "<br>";
+
+    echo "<p>Tertiary School: ";
+    echo $tertiary;
+    echo "<br> Course: "; 
+    echo $course; 
+?>
+
+<h2>FAVORITE FOODS</h2>
+<?php
+    $foods = array
+    (
+        "Sisig",
+        "Pizza",
+        "Sushi",
+        "Takoyaki",
+        "Ramen"
+    );
+    echo "1. $foods[0]<br />";
+    echo "2. $foods[1]<br />";
+    echo "3. $foods[2]<br />";
+    echo "4. $foods[3]<br />";
+    echo "5. $foods[4]<br />";
+?>
+
+<h2>FAVORITE MOVIES</h2>
+<?php
+    $movies = array
+    (
+        "Tamako Love Story",
+        "Koe no Katachi",
+        "Zutto Mae Kara Suki Deshita",
+        "Uchiage Hanabi",
+        "Tenki no Ko"
+    );
+    echo "1. $movies[0]<br />";
+    echo "2. $movies[1]<br />";
+    echo "3. $movies[2]<br />";
+    echo "4. $movies[3]<br />";
+    echo "5. $movies[4]<br />";
+?>
+
+</body>
+</html>
