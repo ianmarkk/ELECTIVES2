e93f182209fc1bef600bd0d1c867b747b91d9e58
Ian Mark Asuncion <99246141+ianmarkk@users.noreply.github.com>
2022-03-07 23:50:14 +0800

    Add files via upload

---
+++ Module02/act4-mod2-diceroll.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dice Rolling</title>
+    </head>
+    <body>
+        <?php
+            $FaceNameSingular = array("one","two","three","four","five","six");
+            $FaceNamePlural = array("ones","twos","threes","fours","fives","sixes");
+
+            function CheckForDoubles($Die1,$Die2)
+            {
+                global $FaceNameSingular;
+                global $FaceNamePlural;
+                $ReturnValue = false;
+        
+                if ($Die1 == $Die2) // Doubles
+                { 
+                    echo "The roll was double",
+                    $FaceNamePlural[$Die1-1], ".<br />";
+                    $ReturnValue = true;
+                }
+                else // Not Doubles
+                {
+                    echo "The roll was a ",
+                    $FaceNameSingular[$Die1-1], " and a ", $FaceNameSingular[$Die2-1], ".<br />";
+                    $ReturnValue = false;
+                }
+                return $ReturnValue;
+            }
+            function DisplayScoreText($Score,$Doubles)
+            {
+                switch ($Score)
+                {
+                    case 2:
+                        echo "You rolled snake eyes! <br />";
+                        break;
+                    case 3:
+                        echo "You rolled a loose deuce!  <br />";
+                        break;
+                    case 5:
+                        echo "You rolled a fever five!  <br />";
+                        break;
+                    case 7:
+                        echo "You rolled a natural!  <br />";
+                        break;
+                    case 9:
+                        echo "You rolled a nina!  <br />";
+                        break;
+        
+                    case 11:
+                        echo "You rolled a yo! <br />";
+                        break;
+                    case 12:
+                        echo "You rolled boxcars! <br />";
+                        break;
+                    default:
+                    if ($Score % 2 == 0)
+                    {
+                        if ($Doubles)
+                        {
+                            echo "You rolled a hard $Score!<br />";
+        
+                        }
+                        else 
+                        {
+                            echo "You rolled an easy $Score!<br />"; 
+                        }
+                    }
+                    break;
+        
+                }
+            }
+            
+            $Dice = array();
+            $Dice[0] = rand(1,6);
+            $Dice[1] = rand(1,6);
+            $Score = $Dice[0] + $Dice[1];
+            echo "<p>";
+            echo "The total score of the roll was: <b>$Score</b>.<br /> ";
+
+            $Doubles = CheckForDoubles($Dice[0],$Dice[1]);
+            DisplayScoreText($Score,$Doubles);
+            echo "<p>";       
+        ?>
+    </body>
+</html>
